44bd657e41f85de91c2571e151fd283702d26bb3
Eyupdzhan Deniz Yalmaz <EyupdzhanDeniz.Yalmaz@VERIFONE.com>
2022-06-18 20:32:59 +0300

    fast added

---
+++ GetVideo/downloader.py
@@ -0,0 +1,11 @@
+# https://tartuulikool-my.sharepoint.com/:v:/g/personal/karlmv_ut_ee/EZ6j72i7Z_pDlzb1EGisfysBxLo0CQLcv_alYyCQB5Q8Rw?e=6OSSoT
+import requests
+
+site_url = 'https://tartuulikool-my.sharepoint.com/:v:/g/personal/karlmv_ut_ee/Eb_w5Gj4N0lIv3sIgmalcjQBWjktodUT3JRiO3GWkRAqCw?e=YfASkt'
+
+URL = site_url + '&download=1'
+
+resp = requests.get(URL) # making requests to server
+
+with open('download.mp4', "wb") as f: # opening a file handler to create new file 
+    f.write(resp.content) 
