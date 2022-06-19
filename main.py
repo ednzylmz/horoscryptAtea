fb407f299f7a1d56c0cbc1250afefab4635b5671
Eyupdzhan Deniz Yalmaz <EyupdzhanDeniz.Yalmaz@VERIFONE.com>
2022-06-19 03:51:09 +0300

    fast added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from functions import *
-from captioning import captioner
+from captioning import *
 from vars import *
+from azureadvanced import *
+
 #Download meeting
 site_url = 'https://tartuulikool-my.sharepoint.com/:v:/g/personal/karlmv_ut_ee/Eb_w5Gj4N0lIv3sIgmalcjQBWjktodUT3JRiO3GWkRAqCw?e=YfASkt'
 mp4 = meeting_downloader(site_url)
 
 mp3 , wav = extract_audio(mp4)
-captioner(textkey, textregion , wav)
+vtt_file = captioner(textkey, textregion , wav)
+
+# summary = summarizer(vtt_file) # for now assume it does work for flask
+summary = './artifacts/summary.txt'
